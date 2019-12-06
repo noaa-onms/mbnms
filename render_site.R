7336884602c723106c447d0f247f2382310923d7
Ben Best <ben@ecoquants.com>
2019-12-06 02:13:51 -0800

    white theme, latest packages

--- render_site.R
+++ render_site.R
@@ -3,19 +3,26 @@ library(here)
 library(readr)
 library(fs)
 library(purrr)
+library(glue)
 
 # parameters
 csv         <- here("svg/svg_links_mbnms.csv")
-redo_modals <- F
+redo_modals <- T
+rmd_skip    <- c("key-climate-ocean.Rmd")
 
 # read in links for svg
 d <- read_csv(csv)
 
-render_modal <- function(input){
-  render(input, html_document(
+render_modal <- function(rmd){
+  render(rmd, html_document(
     theme = site_config()$output$html_document$theme, 
     self_contained=F, lib_dir = here("modals/modal_libs"), 
     mathjax = NULL))
+  
+  htm <- fs::path_ext_set(rmd, "html")
+  docs_htm <- glue("docs/{htm}")
+  docs_rmd <- glue("docs/{rmd}")
+  file.copy(c(rmd, htm), c(docs_htm, docs_rmd), overwrite = T)
 }
 
 # create/render modals by iterating over svg links in csv ----
@@ -33,7 +40,7 @@ for (i in 1:nrow(d)){ # i=1
   } else {
     rmd_newer <- T
   }
-  if (rmd_newer | redo_modals){
+  if ((rmd_newer | redo_modals) & (!basename(rmd) %in% rmd_skip)){
     render_modal(rmd)
   }
 }
@@ -44,6 +51,8 @@ walk(
   list.files(".", "*\\.html$"), 
   function(x) file.copy(x, file.path("docs", x)))
 render_site()
+
+fs::file_touch("docs/.nojekyll")
 
 # shortcuts w/out full render:
 # file.copy("libs", "docs", recursive=T)
